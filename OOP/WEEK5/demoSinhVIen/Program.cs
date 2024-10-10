e3a2894bf2c973e9b4b02eb530ba57dee3d11d25
ppthuyen <thaohuyenthaohuyen123@gmail.com>
2024-10-10 21:27:48 +0700

    up

--- OOP/WEEK5/demoSinhVIen/Program.cs
+++ OOP/WEEK5/demoSinhVIen/Program.cs
@@ -1,1 +1,10 @@
-﻿namespace D
+﻿namespace demoSinhVIen;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SInhVien sv1 = new SInhVien("Lan",5.5);
+        sv1.HienThi();
+    }
+}
